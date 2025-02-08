7c5ed50b0b349b59819cc316ac92a3c83a6a5c83
Nikita Yastreb <yastrebnikita723@gmail.com>
2025-02-09 00:53:46 +0100

    add duckduckgo search tools

--- services/agent_tools/__init__.py
+++ services/agent_tools/__init__.py
@@ -1,13 +1,17 @@
 from .webscraper import webscraper_tool
 from .youtube_search import youtube_search_tool
 from .youtube_transcript_loader import youtube_transcript_loader_tool
+from .duckduckgo_search import duckduckgo_search_tool, duckduckgo_search_results_tool
 
 
-toolkit = [webscraper_tool, youtube_search_tool, youtube_transcript_loader_tool]
+toolkit = [webscraper_tool, youtube_search_tool, youtube_transcript_loader_tool, duckduckgo_search_tool,
+           duckduckgo_search_results_tool]
 
 __all__ = [
     "webscraper_tool",
     "youtube_search_tool",
     "youtube_transcript_loader_tool",
+    "duckduckgo_search_tool",
+    "duckduckgo_search_results_tool",
     "toolkit",
 ]
---
+++ services/agent_tools/duckduckgo_search.py
@@ -0,0 +1,4 @@
+from langchain_community.tools import DuckDuckGoSearchRun, DuckDuckGoSearchResults
+
+duckduckgo_search_tool = DuckDuckGoSearchRun()
+duckduckgo_search_results_tool = DuckDuckGoSearchResults()
